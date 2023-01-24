f6219214ceb9b1e1772dd85bdaab13e56d240d99
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-23 23:29:55 -0300

    Actualizar

--- arrays.js
+++ arrays.js
@@ -1,1 +1,5 @@
-let selectedColors = [];
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = '1'
+console.log(selectedColors[0]);
+console.log(selectedColors[1]);
+console.log(selectedColors[2]);
