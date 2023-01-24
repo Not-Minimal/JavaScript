5cab8f2f7613ce3e6c841d5414c14bf49bef76c1
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-23 23:37:22 -0300

    Funciones en JavaScript

---
+++ funciones.js
@@ -0,0 +1,7 @@
+//Parametro para la funcion greet "name"
+function greet(name, lastName) {
+    console.log('Hola ' + name + ' ' + lastName);
+}
+
+// Argumento para la funcion
+greet('Saul', 'Muñoz');
