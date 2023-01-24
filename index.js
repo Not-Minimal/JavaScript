e5234c5401da8b2df44bd81c7e3669baf31914e3
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-23 23:03:26 -0300

    Subir .js code

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 let name = 'Saul'; //String
 let age = 22; //Number
-let isApproved = false; //Bolean
-let selectedColor = null;
-let firstName = undefined;
+
+let person = {
+    name: 'Ale',
+    age: 22
+};
+//Dot Notation
+person.name = 'Pedro';
+
+//Bracket Notation
+person['name'] = 'Juan';
+
+console.log(person.name);
