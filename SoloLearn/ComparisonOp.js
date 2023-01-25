8a14795492d8013a39f61985a336d62e6ccaab95
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-25 17:34:42 -0300

    OK

---
+++ SoloLearn/ComparisonOp.js
@@ -0,0 +1,4 @@
+//We can use comparison operators in logical statements to find out if variables or values are differents
+//I will get either true or false
+var numero = 10;
+console.log(numero == 8); //False
