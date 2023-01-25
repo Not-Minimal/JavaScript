af94b3557fc7b1cc7f9b7b85b97a17796fc9f422
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-25 17:40:31 -0300

    Operadores de Comparacion

--- SoloLearn/ComparisonOp.js
+++ SoloLearn/ComparisonOp.js
@@ -2,3 +2,13 @@
 //I will get either true or false
 var numero = 10;
 console.log(numero == 8); //False
+/*
+ * == Equal to
+ * === identical in same Type
+ * != not Equal to
+ * !== not identical
+ * > Mayor
+ * < Minor
+ * >= Mayor e igual
+ * <= Menor e Igual
+ */
