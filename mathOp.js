b12b390e069a8e5f33f6f0f315f127bd12fa5f75
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-25 17:20:54 -0300

    Ok

--- mathOp.js
+++ mathOp.js
@@ -4,3 +4,9 @@ var x = 10 + 5;
 document.write(x);
 var y = x + 5 - (22 * 45) / 10;
 document.write(y);
+//Multiplicacion
+var o = 10 * 5;
+document.write(o);
+//Division
+var t = 100 / 5;
+document.write(t);
