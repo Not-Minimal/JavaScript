34c1ce3f34aea83051acf9462bf61dd7d1262171
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-24 22:43:07 -0300

    Subir

---
+++ SoloLearn/SoloLearn.js
@@ -0,0 +1,14 @@
+let curso = "Curso JavaScript";
+console.log("Primer Mensaje por Consola");
+
+/*
+*- JavaScript is a very popular programming language.
+- Web developers use the console to test code and fix bugs.
+- You can use console.log() to write or log messages to the console.
+*/
+//Output
+//<script>
+document.write("Hola Mundo"); //document.write() function. This is what we need to use to write text into our HTML document.
+//</script>
+//
+document.write("<h1>Hola Mundo</h1>"); //document.write() function. This is what we need to use to write text into our HTML document.
