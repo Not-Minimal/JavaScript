8fccad4c6504a6f590dc04d0fcd57987910e1d82
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-25 17:25:35 -0300

    Ok

--- mathOp.js
+++ mathOp.js
@@ -10,3 +10,10 @@ document.write(o);
 //Division
 var t = 100 / 5;
 document.write(t);
+//Modulo
+var modulo = 26 % 6;
+//Incrementar y Decrecer
+//Increment ++
+//The increment operator increases the numeric value of its operand by 1. When placed before the operand, it’ll return the incremented value. When placed after it, it’ll return the original value and then increments the operand.
+//Decrement --
+//The decrement operator decreases the numeric value of its operand by 1. When placed before the operand, it’ll return the decremented value. When placed after the operand, it’ll return the original value and then decrements the operand.
