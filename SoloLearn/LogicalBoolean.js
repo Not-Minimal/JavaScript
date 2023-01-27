18408f251745b750ffd0f6ac60ad2f35493135ab
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-27 16:21:46 -0300

    Logica Booleana

--- SoloLearn/LogicalBoolean.js
+++ SoloLearn/LogicalBoolean.js
@@ -1,3 +1,15 @@
 //&& True if both are true
 //|| true if one of both are true
 //! returns true if its false
+
+var x = 4 > 2 && 10 < 15;
+
+console.log(x);
+
+//Conditional(Ternary)
+//Conditional, or ternay, operators assign a value to a variable, based on some condition.
+//variable = (condicion) ? value1: value2
+//Example:
+var age;
+var isAdult = age < 18 ? "Too young" : "Old enough";
+console.log(isAdult);
