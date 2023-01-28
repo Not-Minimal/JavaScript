e61a09d65dfb907ade72b4b81d415d98b520a435
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-28 12:50:50 -0300

    Ejercicio 5 en JavaScript

---
+++ TranslateCtoJavaScript/Ejercicios/Basicos/ejercicio5.js
@@ -0,0 +1,6 @@
+var x1 = prompt("Ingrese x1 primera coordenada: ");
+var y1 = prompt("Ingrese y1 primera coordenada: ");
+var x2 = prompt("Ingrese x2 segunda coordenada: ");
+var y2 = prompt("Ingrese y2 segunda coordenada: ");
+var resultado = (y2 - y1) / (x2 - x1);
+document.write("La pendiente de la recta entre los puntos es: " + resultado);
