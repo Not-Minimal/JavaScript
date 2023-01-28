d2416f12fc43ec66d8140a52fa114b662b898308
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-27 22:39:06 -0300

    Conditionals en JavaScript

---
+++ SoloLearn/Conditionals.js
@@ -0,0 +1,23 @@
+//The if statement
+//We use if to specify a block of code that we want to be executed if specified condition is true
+
+//if (condition){
+//statement
+//}
+//
+
+var num1 = 7;
+var num2 = 10;
+if (num1 < num2) {
+  console.log("JavasCript is easy to learn.");
+}
+
+//alert() Make an a popup alert box that contains information inside paretheses.
+//
+//
+var month = "september";
+if (month == "August") {
+  console.log("vacation");
+} else {
+  console.log("");
+}
