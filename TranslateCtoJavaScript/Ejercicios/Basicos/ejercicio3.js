31f93bbd5f944b0868d6add82fe088183007a17b
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-28 12:38:02 -0300

    Ejercicio 3 en JavaScript

---
+++ TranslateCtoJavaScript/Ejercicios/Basicos/ejercicio3.js
@@ -0,0 +1,6 @@
+var a = prompt("Ingrese el valor de a: ");
+var b = prompt("Ingrese el valor de b: ");
+a = a * a * a * 5;
+b = b * b * 2;
+var resultado = a + b - 30;
+document.write("El resultado de la ecuacion es: " + resultado);
