fc4a2fa48cfbd19085379c424c000231e7bc6401
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-28 12:17:15 -0300

    Ejercicio 1 en JavaScript

---
+++ TranslateCtoJavaScript/Ejercicios/Basicos/ejercicio1.js
@@ -0,0 +1,5 @@
+//Calcular IVA de un producto
+//Usar prompt para solicitar un valor por pantalla
+var a = prompt("Ingrese el valor del producto");
+var b = a * 0.19;
+document.write("El iva del producto es: " + b);
