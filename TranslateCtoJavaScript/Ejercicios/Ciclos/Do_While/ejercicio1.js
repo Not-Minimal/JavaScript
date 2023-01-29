293091ec4ac4043b80964b11435a4449e1ff5a3a
Saul Muñoz <sam.pedreros14@gmail.com>
2023-01-28 21:13:24 -0300

    Ejercicio 1 con ciclo DoWhile en JavaScript

--- TranslateCtoJavaScript/Ejercicios/Ciclos/Do_While/ejercicio1.js
+++ TranslateCtoJavaScript/Ejercicios/Ciclos/Do_While/ejercicio1.js
@@ -2,4 +2,4 @@ do {
   var neto = prompt("Ingrese el valor neto: ");
   var total = neto * 1.19;
 } while (neto <= 180);
-document.write("El valor del producto es: " + total);
+document.write("El valor del producto con iva es: " + total);
