53f23d55683f45ef5179794b3a7b8f1aaba39b57
Saul Muñoz <sam.pedreros14@gmail.com>
2023-02-01 16:35:21 -0300

    Actualizar

--- funciones.js
+++ funciones.js
@@ -1,17 +1,18 @@
 function greet(name, lastName) {
   if (name && lastName) {
-      console.log(`Hola ${name} ${lastName}`);
+    console.log(`Hola ${name} ${lastName}`);
   } else {
-      console.log("Por favor proporciona un nombre y apellido.")
+    console.log("Por favor proporciona un nombre y apellido.");
   }
 }
 
 // Argumento para la funcion
-greet('Saul', 'Muñoz');
+greet("Saul", "Muñoz");
 
 //Calcular el cuadrado de un numero
 function elevarAlCuadrado(number) {
-return number * number;
+  return number * number;
 }
 
 console.log(elevarAlCuadrado(3));
+console.log(elevarAlCuadrado(3) * elevarAlCuadrado(3));
