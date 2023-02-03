df773badfcb97cb767beb98c30d1644dceeb4ffb
Saul Muñoz <sam.pedreros14@gmail.com>
2023-02-03 13:50:01 -0300

    Ejercicio con Objetos en JS

---
+++ SoloLearn/Objects.js
@@ -0,0 +1,17 @@
+//JavaScript variables are containers for data values.
+//Objects are variables too, but they can contain many values.
+//Think of an object as a list of values that are written as
+//name:value pairs, with the names and the values separated by colons.
+var person = {
+  name: "Saul",
+  age: 22,
+  color: "Red",
+  altura: 173,
+};
+//Its could access object properties in two ways
+//objectName.propertyName
+//objectName['propertyName']
+var x = person.age;
+var y = person["age"];
+var course = { name: "JS", lessons: 41 };
+document.write(course.name.length);
