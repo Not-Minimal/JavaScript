bcc978ea741eb186b86682d7b26286eeeda8d4fd
Saul Muñoz <sam.pedreros14@gmail.com>
2023-02-09 02:14:01 -0300

    Arrays en JS

---
+++ SoloLearn/Array.js
@@ -0,0 +1,12 @@
+//Arrays strore multiple values in a single variable
+//var course1 = "HTML";
+//var course3 = "CSS";
+//var course4 = "JS";
+//But if theres more data?
+var courses = new Array("HTML", "CSS", "JS");
+//Accessing an Array
+//Your refer to an array element by referring to the index number written in square brackets.
+//This statemen accessses the value of the first element in courses and changes the value of the secod element.
+var courses = new Array("JS", "HTML", "C");
+var course = course[0]; //JS
+courses[1] = "C++"; //Changes HTML to C++
