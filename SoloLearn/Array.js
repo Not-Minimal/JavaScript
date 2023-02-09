4a44de0cbe980c2ce7e3088c7b530d9af559ac9a
Saul Muñoz <sam.pedreros14@gmail.com>
2023-02-09 02:23:36 -0300

    Arrays in JS

--- SoloLearn/Array.js
+++ SoloLearn/Array.js
@@ -10,3 +10,8 @@ var courses = new Array("HTML", "CSS", "JS");
 var courses = new Array("JS", "HTML", "C");
 var course = course[0]; //JS
 courses[1] = "C++"; //Changes HTML to C++
+//It can aldo declare an array, tell it the number of element it will store, and add the elements later.
+var cursos = new Array(3);
+cursos[0] = "GO";
+cursos[1] = "Dart";
+cursos[2] = "Ruby";
