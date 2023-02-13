89dbfd2e4fe76850170e49b7baf287f042b10ec7
Saul Muñoz <sam.pedreros14@gmail.com>
2023-02-13 10:18:51 -0300

    Arrays y asociacividad en JS

---
+++ SoloLearn/AssociativeArrays.js
@@ -0,0 +1,7 @@
+//While many programming languages support arrays with named indexes(
+//text instead of numbers), called associative arrays JavaScript does not.
+//However, you still can use the named array syntax, which will produce an object.
+var person = []; //Empty Array
+person["name"] = "Saul";
+person["age"] = 22;
+console.log(person["age"]);
