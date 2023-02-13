e062828b0666d030b6704ffe5ef1c4a4f33d4e73
Saul Muñoz <sam.pedreros14@gmail.com>
2023-02-13 10:22:34 -0300

    Math in JS

---
+++ SoloLearn/MathObject.js
@@ -0,0 +1,7 @@
+//E Eulers constant
+//LN2 Natural Log of the value 2
+//LN10 Natural Log of the value 10
+//LOG2E The base 2 log od Eulers constan (E)
+//LOG10E The base 10 log od Eulers constan (E)
+//PI Return the constant PI
+console.log(Math.PI);
