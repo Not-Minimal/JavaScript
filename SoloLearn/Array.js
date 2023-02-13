3176480706425106e86dc57c1576c3e64981696d
Saul Muñoz <sam.pedreros14@gmail.com>
2023-02-13 10:01:57 -0300

    Actualizar

--- SoloLearn/Array.js
+++ SoloLearn/Array.js
@@ -8,10 +8,14 @@ var courses = new Array("HTML", "CSS", "JS");
 //Your refer to an array element by referring to the index number written in square brackets.
 //This statemen accessses the value of the first element in courses and changes the value of the secod element.
 var courses = new Array("JS", "HTML", "C");
-var course = course[0]; //JS
+
 courses[1] = "C++"; //Changes HTML to C++
 //It can aldo declare an array, tell it the number of element it will store, and add the elements later.
 var cursos = new Array(3);
 cursos[0] = "GO";
 cursos[1] = "Dart";
 cursos[2] = "Ruby";
+
+var nuevo = courses.concat(cursos);
+console.log(nuevo);
+
